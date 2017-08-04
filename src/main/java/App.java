47522a75dacb67a196f90e09b0d29e9e8d1eaed5
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-04 11:23:12 -0700

    add more user prompts

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,15 +11,16 @@ public class App {
         boolean programRunning = true;
 
         try {
-            while (programRunning){
+            while (programRunning) {
                 System.out.println("Welcome to the Pink Footed Dinosaur's Boutique event planning!");
                 System.out.println("We're so glad you chose us to cater your super special event!");
                 System.out.println("Would you like to book a reservation?(yes or no)");
                 String answer = bufferedReader.readLine();
-                if (answer.equalsIgnoreCase("yes")){programRunning = true;
+                if (answer.equalsIgnoreCase("yes")) {
+                    programRunning = true;
                     System.out.println("Great! Let's begin!");
                     System.out.println("##########################################################");
-                }else if(answer.equalsIgnoreCase("no")){
+                } else if (answer.equalsIgnoreCase("no")) {
                     programRunning = false;
                     System.out.println("Thanks for visiting! We hope to see you again!");
                     System.out.println("##########################################################");
@@ -27,19 +28,25 @@ public class App {
                 System.out.println("##########################################################");
                 System.out.println("How many guests do you plan on hosting? At current, we're only able to host up to 1000 guests and our minium is 10");
                 int guests = Integer.parseInt(bufferedReader.readLine());
-                if(guests >1000|| guests < 10){
+                if (guests > 1000 || guests < 10) {
                     System.out.println("We won't be able to accomodate that many people, Have a great day!");
                     System.out.println("##########################################################");
-                }
-                System.out.println("Great! You'd like us to provide the decorations, right?(true or false)");
-                boolean party = Boolean.parseBoolean(bufferedReader.readLine());
-                System.out.println("Great! Now as for food! Here are our options. Please pick one!");
-                System.out.println("");
+                } else {
+                    System.out.println("Great! You'd like us to provide the decorations, right?(true or false)");
+                    boolean party = Boolean.parseBoolean(bufferedReader.readLine());
+                    System.out.println("##########################################################");
+                    System.out.println("Great! Now as for food! Here are our options. Please pick one!");
+                    System.out.println("none, snack, family style, food stations, buffet, brunch, dinner");
+                    String food = bufferedReader.readLine();
+                    System.out.println("##########################################################");
+                    System.out.println("Great! Now we all know no party is complete without music! Here are our options. Please pick one!");
+                    System.out.println("none, equipment only, dj, band");
+                    String music = bufferedReader.readLine();
+
 //                Party newParty = new Party(guests; String music; String food; String beverages; decor );
+                }
+
             }
-
-
-
 
         } catch (IOException e) {
             e.printStackTrace();
